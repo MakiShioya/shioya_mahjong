3ef89e2b8f697880f377e8d4a0c300a581b9482b
MakiShioya <makimakisioya@gmail.com>
2026-02-21 19:19:19 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html', // ロビー
-        game: 'game.html',  // 対局
-        home: 'home.html'   // ホーム
+        main: 'index.html',  // タイトル画面（今回作るもの）
+        home: 'home.html',   // ホーム画面
+        lobby: 'lobby.html', // ロビー画面
+        game: 'game.html'    // 対局画面
       }
     }
   }
