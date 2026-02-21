baba562a3ed68f53bd893d9930349994e23a9e1a
MakiShioya <makimakisioya@gmail.com>
2026-02-21 19:11:52 +0900

    Simplify paths in Vite config for HTML inputs

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'), // 現在はロビー、後でホーム画面に
-        game: resolve(__dirname, 'game.html')   // 対局画面
+        main: 'index.html',
+        game: 'game.html'
       }
     }
   }
